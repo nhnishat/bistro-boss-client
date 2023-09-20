45a2f066f5343081a890e86c46f641ecf17f63de
nhnishat <nhnishat1998@gmail.com>
2023-09-21 01:43:20 +0600

    redirect

--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -5,11 +5,15 @@ const PrivateRoute = ({ children }) => {
 	const { user, loading } = useAuth();
 	const location = useLocation();
 	if (loading) {
-		return <span className="loading loading-spinner loading-lg"></span>;
+		// return <span className="loading loading-spinner loading-lg"></span>;
+		return <Navigate to="/login"></Navigate>;
 	}
 	if (user) {
 		return children;
 	}
+	// if (!user) {
+	// 	return <Navigate to="/login"></Navigate>;
+	// }
 
 	return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
